69588f202e3932ee30885075a8dad3bf3f724a2a
kyrgyzCoder <Nurmuhammed.shadiev@gmail.com>
2019-08-16 09:44:58 -0700

    This is done

--- ex00/ft_split_whitespaces.c
+++ ex00/ft_split_whitespaces.c
@@ -5,64 +5,73 @@ int	get_word_count(char *str)
 {
 	int	counter;
 	int	i;
+	int	add;
 
+	if (!str)
+		return (0);
 	i = 0;
 	counter = 1;
 	while (str[i] != '\0')
 	{
-		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n')
-			counter++;
-		i++;
+		while (str[i] != ' ' && str[i] != '\t' && str[i] != '\n' && str[i] != '\0')
+		{
+			i++;
+			add = 1;
+		}
+		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == '\0')
+		{
+			if (sp == 1)
+				count++;
+			add = 0;
+			i++;
+		}
 	}
 	return (counter);
 }
 
-int	get_len(char *str)
+int	get_len(char *str, int i)
 {
 	int	len;
-	int	i;
 
 	len = 0;
-	i = 0;
 	while (str[i] != ' ' && str[i] != '\t' &&
 			str[i] != '\n' && str[i] != '\0')
 	{
 		len++;
 		i++;
 	}
-	return (++len);
+	return (len);
 }
 
-char	**line_subtr(char *str, char *temp, int len, int row, int i)
+char	**line_subtr(char *str, char *temp, int row, int i, int j)
 {
 	char	**res;
+	int	k;
 
         row = get_word_count(str);
-        while (i < row)
+        res = (char *)malloc(sizeof(char) * row);
+	if (res == NULL)
+		return (NULL);
+        while (str[i])
         {
-                len = get_len(temp);
-                temp = temp + len;
-                res[i++] = (char *)malloc(sizeof(char) * len);
+                if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n')
+			i++;
+		if (str[i])
+		{
+			k = 0;
+			res[j] = (char *)malloc(sizeof(char) * (get_len(str, i) + 1));
+			if (res[j] == NULL)
+				return (NULL);
+			while (str[i] != '\t' && str[i] != ' ' && str[i] != '\n' && str[i])
+				res[j][k++] = str[i++];
+			res[j++][k] = '\0';
+		}
         }
-        res[i] = (char *)malloc(sizeof(char) * 2);
-        i = 0;
-        len = 0;
-        while (*str)
-        {
-                if (*str == ' ' || *str == '\t' || *str == '\n')
-                {
-                        res[i++][len] = '\0';
-                        str++;
-                        len = 0;
-                }
-                res[i][len++] = *str++;
-        }
-        res[i + 1][0] = '0';
-        res[i + 1][1] = '\0';
+        res[j] = "0";
         return (res);
 }
 
 char	**ft_split_whitespaces(char *str)
 {
-	return (line_subtr(str, str, len, row, i);
+	return (line_subtr(str, str, 0, 0, 0);
 }
