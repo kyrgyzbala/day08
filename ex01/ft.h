5ca686e797305bdfb0dd942d4249f1b88fc63ad9
kyrgyzCoder <Nurmuhammed.shadiev@gmail.com>
2019-08-16 09:18:46 -0700

    Done

--- ex01/ft.h
+++ ex01/ft.h
@@ -1,3 +1,6 @@
+#ifndef __FT_H__
+#define __FT_H__
+
 void	ft_putchar(char ch);
 
 void	ft_swap(int *a, int *b);
@@ -7,3 +10,5 @@ void	ft_putstr(char *str);
 void	ft_strlen(char *str);
 
 void	ft_strcmp(char *s1, char *s2);
+
+#endif
