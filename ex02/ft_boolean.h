31c60da20aa382606bd0989274fa2fe46197d9f7
kyrgyzCoder <Nurmuhammed.shadiev@gmail.com>
2019-08-16 09:48:14 -0700

    Finished

--- ex02/ft_boolean.h
+++ ex02/ft_boolean.h
@@ -5,8 +5,8 @@ int	write(int n, void *str,unsigned int size);
 
 #define TRUE 1
 #define FALSE 0
-#define EVEN_MSG "I have an even number of arguments"
-#define ODD_MSG "I have an odd number of arguments"
+#define EVEN_MSG "I have an even number of arguments.\n"
+#define ODD_MSG "I have an odd number of arguments.\n"
 #define SUCCESS 0
 #define EVEN(nbr) (nbr % 2 == 0)
 #define t_bool int;
